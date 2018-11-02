3482e464ac0f4b94d19822755e09dcac26abce9e
sauravchandra1 <sauravchandra125@gmail.com>
2018-11-02 14:55:40 +0530

    Update 02/11/2018

--- main.cpp
+++ main.cpp
@@ -12,17 +12,18 @@ int main() {
         cin >> arr[i];
     if (arr[0] == -1) 
     	for (int i = 1; i <= A; i++)
-    		dp[0][i][1] = 1;
+    		dp[0][i][0] = 1;
     else
-    	dp[0][arr[0]][1] = 1;
+    	dp[0][arr[0]][0] = 1;
     for (int i = 1; i < n; i++) {
     	if (arr[i] == -1) {
     		for (int j = 2; j <= A; j++)
-    			dp[i][j][0] = (dp[i][j][0] % MOD + dp[i][j - 1]
-    						dp[i - 1][j][0] % MOD + dp[i - 1][j][1] % MOD) % MOD;
-    		dp[i][1][1] = dp[i - 1][1][1];
-    		for (int j = 2; j <= A; j++) 
-    			dp[i][j][1] = (dp[i][j][1] % MOD + dp[i - 1][j][1] % MOD) % MOD;
+    			dp[i][j][0] = (dp[i][j - 1][0] % MOD + dp[i - 1][j - 1][0] % MOD + dp[i - 1][j - 1][1] % MOD) % MOD;
+    		dp[i][A][1] = dp[i - 1][A][1];
+    		for (int j = A - 1; j >= 1; j--) 
+    			dp[i][j][1] = (dp[i][j + 1][1] % MOD + dp[i - 1][j][1] % MOD) % MOD;
+    		for (int j = A; j >= 1; j--)
+    			dp[i][j][1] = (dp[i][j][1] % MOD + dp[i - 1][j][0] % MOD) % MOD; 
     	}
     	else {
     		int a = arr[i];
@@ -37,7 +38,7 @@ int main() {
     long long ans = 0;
    	if (arr[n - 1] == -1) 
    		for (int i = 1; i <= A; i++)
-   			ans = (ans % MOD + dp[n - 1][i][1] % MOD);
+   			ans = (ans % MOD + dp[n - 1][i][1] % MOD) % MOD;
    	else
    		ans = dp[n - 1][arr[n - 1]][1];
    	cout << ans << endl;
